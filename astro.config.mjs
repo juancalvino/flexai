b0626046dda142599a0df6f553950f31b691d487
jcalvino <juan.m.calvino@gmail.com>
2025-01-16 13:32:31 -0300

    add: vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from '@astrojs/vercel'
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  output: 'server',
+  adapter: vercel(),
+  site: "https://flexai.com.ar",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
